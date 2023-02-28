d28c2270591ec6fb555ef63dd5f837e2054f2df8
Anthony <113257722+AnthonyGouffier@users.noreply.github.com>
2023-02-28 09:25:38 +0100

    Ajout de fichier tier
    Fichier CSV & blibli pour fonctionnement de database.c

--- src/commun.h
+++ src/commun.h
@@ -1,1 +1,5 @@
 #include <stdbool.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
---
+++ src/entites.h
@@ -0,0 +1,41 @@
+#include "commun.h"
+
+typedef bool objet;
+
+typedef struct pokemon_s
+{   
+    int id;
+    char name[64];
+    /*Stats*/
+    int pv_max;
+    int pv;
+    int total;
+    float att;
+    float def;
+    float attspe;
+    float defspe;
+    float spd;
+    int range;
+    /*autre*/
+    char classe[20];
+    int rarete;
+    int stade;
+    bool alive;
+    /*partie graphique*/
+    void * img;
+    /*coordonne*/
+    int x , y;
+}pokemon_t;
+
+typedef struct player_s
+{
+    int hp;
+    int niveau;
+    pokemon_t *team;
+    objet * objListe ;
+    /*status*/
+    int money;
+    int interest;
+    bool alive;
+}player_t;
+
