4d701627e8b5adf63a54325a53856b72cfc26b1f
mael/gaumont <gaumonediedemone@gmail.com>
2023-03-16 11:38:04 +0100

    mise_a_jour

--- src/combats.c
+++ src/combats.c
@@ -82,8 +82,8 @@ void effacer(pok_t mat[N][N],pos_t pos){
 
 
 void deplacement_pok(pok_t mat[N][N],pos_t pos,pos_t newpos){
-      int i=newpos.x,j=newpos.y;
-      int x=pos.x,y=pos.y;
+    int i=newpos.x,j=newpos.y;
+    int x=pos.x,y=pos.y;
       mat[i][j].life=mat[x][y].life;
       mat[i][j].equipe=mat[x][y].equipe;
       mat[i][j].hp=mat[x][y].hp;
@@ -95,7 +95,8 @@ void deplacement_pok(pok_t mat[N][N],pos_t pos,pos_t newpos){
       mat[i][j].evol=mat[x][y].evol;
       mat[i][j].tier=mat[x][y].tier;
       effacer(mat,pos);
-
+      pos.x=newpos.x;
+      pos.y=newpos.y;
 }
 
 
@@ -244,6 +245,7 @@ void affiche_stat(pok_t mat[N][N],pos_t pos){
   ,mat[i][j].porter
   ,mat[i][j].evol
   ,mat[i][j].tier);
+  printf(" \n ");
 
 }
 
@@ -265,33 +267,30 @@ int main(){
 
     pok_t mat[N][N];
     initialise(mat);
-    pos_t test75;
-    test75.x =7;
-    test75.y =7; 
-    affiche_stat(mat,test75);
     affiche_tout(mat);
-    mat[5][5].equipe=pok;
-    mat[5][5].hp=50;
-    mat[5][5].attaque=9;
-    mat[5][5].armor=30;
-    mat[5][5].x=5;
-    mat[5][5].y=5;
-    mat[5][5].porter=2;
-    mat[5][5].evol=1;
-    mat[5][5].tier=2;
-    mat[5][5].life=1;
-
-    mat[1][1].equipe=enemi;
-    mat[1][1].hp=50;
-    mat[1][1].attaque=9;  
-    mat[1][1].armor=30;
-    mat[1][1].x=1;
-    mat[1][1].y=1;
-    mat[1][1].porter=2;
-    mat[1][1].evol=1;
-    mat[1][1].tier=2;
-    mat[1][1].life=1;
-
+    int g1=4,g2=4;
+    mat[g2][g1].equipe=pok;
+    mat[g2][g1].hp=50;
+    mat[g2][g1].attaque=9;
+    mat[g2][g1].armor=30;
+    mat[g2][g1].x=g2;
+    mat[g2][g1].y=g1;
+    mat[g2][g1].porter=2;
+    mat[g2][g1].evol=1;
+    mat[g2][g1].tier=2;
+    mat[g2][g1].life=1;
+    int h2=2,h1=2;
+    mat[h2][h1].equipe=enemi;
+    mat[h2][h1].hp=50;
+    mat[h2][h1].attaque=9;  
+    mat[h2][h1].armor=30;
+    mat[h2][h1].x=h2;
+    mat[h2][h1].y=h1;
+    mat[h2][h1].porter=2;
+    mat[h2][h1].evol=h1;
+    mat[h2][h1].tier=2;
+    mat[h2][h1].life=1;
+    affiche_tout(mat);
     PL_t player1;
     PL_t player2;
     player1.hp=100;
@@ -303,18 +302,33 @@ int main(){
     pos_t pok1;
     pos_t enemi_proche;
     pos_t testdep;
-    enemi1.x =0;
-    enemi1.y =0;
-    pok1.x =5;
-    pok1.y =2;
+    pos_t testdep2;
+    enemi1.x =h2;
+    enemi1.y =h1;
+    pok1.x =g2;
+    pok1.y =g1;
+    testdep.x=4;
+    testdep.y=4;
+    testdep2=enemi1;
     int cpt = player1.level;
     int k,j,i,fin=0;
 
 
     affiche_test(mat);      //sauv de base + de debut de phase
-
-
-
+    affiche_stat(mat,enemi1);
+    for(int r=0;r<N;r++){
+      for(int r2=0;r2<N;r2++){
+        affiche_stat(mat,testdep);
+        testdep2=testdep;
+        testdep.x=r;
+        testdep.y=r2;
+        deplacement_pok(mat,testdep2,testdep);
+        affiche_test(mat); 
+        affiche_stat(mat,testdep);
+      }
+    }
+    
+    /*
     for(k=0;(k<20 || fin==1);k++){    //timer de 20 coup ou s arrete a fin
       printf("Temp=%d \n ",k);
       for(i=0;i<N; i++){
@@ -328,13 +342,14 @@ int main(){
             printf("\n %d %d ",enemi_proche.y,enemi_proche.x);
             if(aporter(mat,mat[i][j].porter,i,j,enemi_proche)){
               printf("aporter");
+              printf(" \n hp = %d",mat[i][j].hp);
               mat[i][j].hp=degats_subi_pkm(mat[i][j].hp,mat[i][j].armor,mat[i][j].attaque);
+              printf(" \n hp = %d",mat[i][j].hp);
             }else{
               printf("avance");
               avance(mat,i,j,enemi_proche.x,enemi_proche.y);
               testdep.x=i;
               testdep.y=j;
-              affiche_stat(mat,testdep);
             }
           }
         }
@@ -342,6 +357,7 @@ int main(){
       for(i=0; i<N; i++){
         for(j=0;j<N;j++){
           if(mat[i][j].hp<=0 && mat[i][j].equipe==pok){
+            printf("OK");
             mat[i][j].life=0;
             cpt--;
           }
@@ -356,5 +372,6 @@ int main(){
     }
 
     printf("fini");
+    */
     return 0;
 }
