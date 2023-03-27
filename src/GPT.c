4f2a5a461186eefc8e1dc7775b422407ec9b23c4
Anthony Gouffier <gouffier.anthony@gmail.com>
2023-03-27 17:36:10 +0200

    fichier pour test

--- src/GPT.c
+++ src/GPT.c
@@ -1,58 +1,32 @@
 #include <SDL.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <SDL_image.h>
 
 int main(int argc, char* argv[]) {
-    /*METTRE LES PARAMS A INIATILISER POUR LA SDL*/
-    if (0 != SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
-        fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError());
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
 
-    SDL_Window* fenetre = SDL_CreateWindow("Ma fenêtre SDL",
-                                            SDL_WINDOWPOS_CENTERED,
-                                            SDL_WINDOWPOS_CENTERED,
-                                            640, 480,
-                                            SDL_WINDOW_SHOWN);
-    if (fenetre == NULL) {
-        fprintf(stderr, "Erreur lors de la création de la fenêtre : %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
+    SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Surface* surface = SDL_CreateRGBSurface(0, 640, 480, 32,
-                                                0x00FF0000,
-                                                0x0000FF00,
-                                                0x000000FF,
-                                                0xFF000000);
-    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 255, 0, 0));
+    SDL_Window* window = SDL_CreateWindow("SDL Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
 
-    if (surface == NULL) {
-        fprintf(stderr, "Erreur lors de la création de la surface : %s\n", SDL_GetError());
-        SDL_DestroyWindow(fenetre);
-        SDL_Quit();
-        return 1;
-    }
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Rect position = { 0, 0, 0, 0 };
-    SDL_BlitSurface(surface, NULL, SDL_GetWindowSurface(fenetre), &position);
-    SDL_UpdateWindowSurface(fenetre);
+    SDL_Surface* image = IMG_Load("example.png");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
 
-    SDL_Event evenement;
-    int continuer = 1;
+    SDL_FreeSurface(image);
 
-    while (continuer) {
-        while (SDL_PollEvent(&evenement)) {
-            if (evenement.type == SDL_QUIT) {
-                continuer = 0;
-            }
-        }
-    }
+    SDL_RenderClear(renderer);
 
-    SDL_FreeSurface(surface);
-    SDL_DestroyWindow(fenetre);
-    
+    SDL_Rect rect = {0, 0, 640, 480};
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000);
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
     SDL_Quit();
 
     return 0;
