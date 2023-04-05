b6490ac2a19fb48430cbfd27801af0163f3de6ce
mael/gaumont <gaumonediedemone@gmail.com>
2023-04-05 14:01:26 +0200

    mise_a_jour

---
+++ src/combats.h
@@ -0,0 +1,179 @@
+/**
+ * \file combats.h
+ * @author mael (you@domain.com)
+ * @brief le combat ptdr
+ * @version 0.1
+ * @date 2023-04-03
+ * 
+ * 
+ */
+#ifndef COMBATS_H
+#define COMBATS_H
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+
+#define pok 1
+#define enemi 2
+#define N 7
+
+typedef struct pos_s{
+  int y;
+  int x;
+}pos_t;
+
+typedef struct PL_s{
+  int hp;
+  int level;
+}PL_t;
+
+
+
+
+
+/**
+ * @brief tier de degat
+ * 
+ */
+int tier_damage[3][5] = {   //tier de damage a infliger au joueur
+  {1,1,1,2,3},
+  {2,2,2,3,4},
+  {3,3,3,5,8}
+};
+
+
+/**
+ * @brief verifi si dans la matrice
+ * 
+ * @param x 
+ * @param y 
+ * @return int 
+ */
+int valides(int x,int y);
+
+/**
+ * @brief calcule les degat a infliger a un pokemon
+ * 
+ * @param hp 
+ * @param armor 
+ * @param attaque 
+ * @return int 
+ */
+int degats_subi_pkm(int hp,int armor,int attaque);
+
+/**
+ * @brief si il est mort 
+ * 
+ * @param hp 
+ * @return int 
+ */
+int est_mort_pkm(int hp);
+
+/**
+ * @brief verifi si il a la porter renvoi 1 sinon 0
+ * 
+ * @param mat 
+ * @param porter 
+ * @param xpos 
+ * @param ypos 
+ * @return int 
+ */
+int aporter(pokemon_t mat[N][N],int porter,int xpos,int ypos);
+
+
+
+/**
+ * @brief efface un pokemon en mettan toute les stat a 0
+ * 
+ * @param mat 
+ * @param pos 
+ */
+void effacer(pokemon_t mat[N][N],pos_t pos);
+
+
+/**
+ * @brief deplace un pokemon d une case a une case
+ * 
+ * @param mat 
+ * @param pos 
+ * @param newpos 
+ */
+void deplacement_pok(pokemon_t mat[N][N],pos_t pos,pos_t newpos);
+
+
+/**
+ * @brief avance un pokemon ver un enemie de 1
+ * 
+ * @param mat 
+ * @param xenemie 
+ * @param yenemie 
+ * @param posx 
+ * @param posy 
+ * @return pos_t 
+ */
+pos_t avance(pokemon_t mat[N][N],int xenemie,int yenemie,int posx,int posy);
+
+/**
+ * @brief detecte le premiere enemie le plus proche et renvoi ces cordone
+ * 
+ * @param pos 
+ * @param mat 
+ * @param equipe 
+ * @return pos_t 
+ */
+pos_t detecte_enemie_proche(pos_t pos,pokemon_t mat[N][N],int equipe);
+
+
+/**
+ * @brief degats infliger a un joueur 
+ * 
+ * @param mat 
+ * @param stage 
+ * @param hp 
+ * @return int 
+ */
+int dmg_player(pokemon_t mat[N][N],int stage,int hp);
+
+/**
+ * @brief recupere et retoune les hp d un pokemon a des cordonner donner
+ * 
+ * @param mat 
+ * @param x 
+ * @param y 
+ * @return int 
+ */
+int recuperehp(pokemon_t mat[N][N],int x,int y);
+
+/**
+ * @brief affiche le test (beta)
+ * 
+ * @param automate 
+ */
+void affiche_test(pokemon_t automate[N][N]);
+
+/**
+ * @brief initialise a zero tout les stat de tout les case et -1 pour life 
+ * 
+ * @param mat 
+ */
+void initialise(pokemon_t mat[N][N]);
+
+
+/**
+ * @brief affiche les stat d un pokemon a des cordonner donner
+ * 
+ * @param mat 
+ * @param pos 
+ */
+void affiche_stat(pokemon_t mat[N][N],pos_t pos);
+
+/**
+ * @brief donne tout les stat des pokemon de la matrice 
+ * 
+ * @param mat 
+ */
+void affiche_tout(pokemon_t mat[N][N]);
+
+#endif
